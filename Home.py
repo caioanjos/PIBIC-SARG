bf05a71752a1a40acaa082181c52758b3c00787c
caio vinicius <122832900+caioanjos@users.noreply.github.com>
2025-08-29 11:32:58 -0300

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Meu Projeto", layout="wide")
+
+st.title("Estudo da história natural e efeitos adversos da epidemia de COVID-19 na saúde da população de um centro urbano de médio porte")
+st.write("""
+Bem-vindo ao aplicativo do meu projeto!  
+Use o menu lateral para navegar entre:
+- **Análise Descritiva**
+- **Índices de Moran, Mapas Interativos e Séries Temporais**
+""")
