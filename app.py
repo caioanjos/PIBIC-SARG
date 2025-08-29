c933ee931fc89b442c3768151f18e8f0ed19552d
caio vinicius <122832900+caioanjos@users.noreply.github.com>
2025-08-29 11:27:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+st.set_page_config(page_title="Meu Primeiro App", layout="wide")
+
+st.title("🌍 Olá, este é meu primeiro app com Streamlit!")
+st.write("Se você está vendo isso online, deu certo ✅")
