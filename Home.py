2c63359eb204544381fb2fb0cc1564e0f9a590a5
caio vinicius <122832900+caioanjos@users.noreply.github.com>
2025-08-29 11:37:38 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,9 +3,14 @@ import streamlit as st
 st.set_page_config(page_title="Meu Projeto", layout="wide")
 
 st.title("Estudo da história natural e efeitos adversos da epidemia de COVID-19 na saúde da população de um centro urbano de médio porte")
+
 st.write("""
-Bem-vindo ao aplicativo do meu projeto!  
-Use o menu lateral para navegar entre:
-- **Análise Descritiva**
-- **Índices de Moran, Mapas Interativos e Séries Temporais**
+Seja bem vindo ao meu Projeto de Pesquisa.  
+
+Aqui você vai encontrar duas partes principais:
+
+- **Análise Descritiva**: explore os dados brutos e veja estatísticas básicas.  
+- **Índices de Moran, Mapas Interativos e Séries Temporais**: análises espaciais e temporais mais avançadas.  
+
+Use o menu lateral (à esquerda) para navegar entre as páginas.
 """)
